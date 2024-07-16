af2f59d062da15cd3e75d80600b234cb947583d8
Lee gun woo <139452702+gunnu3226@users.noreply.github.com>
2024-07-16 13:03:20 +0900

    init: init project

---
+++ src/main/java/gunnu/stocknote/StocknoteApplication.java
@@ -0,0 +1,13 @@
+package gunnu.stocknote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocknoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StocknoteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gunnu/stocknote/StocknoteApplicationTests.java
@@ -0,0 +1,13 @@
+package gunnu.stocknote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StocknoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
